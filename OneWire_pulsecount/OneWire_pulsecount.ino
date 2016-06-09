737a1dd9daa599f3e3d9aaec59b93ade15575a4b
kaas3000 <k_meliefste@hotmail.nl>
2016-06-09 12:30:50 +0200

    WTF pulsecount

--- OneWire_pulsecount/OneWire_pulsecount.ino
+++ OneWire_pulsecount/OneWire_pulsecount.ino
@@ -10,6 +10,13 @@ int pulsesDetected;
 unsigned long pulseStart;
 boolean previousValue;
 
+int leds[] = {
+  2, 3, 4, 5
+};
+int switches[] = {
+  6, 7, 8, 12
+};
+
 void setup() {
   pinIn = A0;
   pinOut = A1;
@@ -21,6 +28,13 @@ void setup() {
 
   pinMode(pinIn, INPUT);
   pinMode(pinOut, OUTPUT);
+  for (int i = 0; i < 4; i++) {
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], OWLOW);
+  }
+  for (int i = 0; i < 4; i++) {
+    pinMode(switches[i], INPUT);
+  }
 }
 
 void loop() {
@@ -31,7 +45,7 @@ void loop() {
     boolean syncPulse = false;
 
     // rising or falling edge detected
-    if (currentTime - pulseStart >= 15 && currentValue == OWLOW) {
+    if (currentTime - pulseStart >= 15 && currentValue == OWHIGH) {
       syncPulse = true;
       Serial.println("Sync pulse detected :)");
       Serial.println(pulsesDetected);
@@ -40,9 +54,46 @@ void loop() {
       pulseStart = millis();
     }
 
-    if (currentValue == OWHIGH && !syncPulse) {
+
+    if (currentValue == OWLOW && !syncPulse) {
       pulsesDetected++;
       pulseStart = millis();
+
+      if(pulsesDetected >= 1 && pulsesDetected <= 4){
+        boolean switchValue = digitalRead(switches[(pulsesDetected % 4)]);
+        pinMode(pinOut, OUTPUT);
+        // digitalWrite(pinOut,OWLOW);
+        delay(2);
+        digitalWrite(pinOut,!switchValue);
+        delay(6);
+
+        pinMode(pinOut, INPUT);
+        // digitalWrite(pinOut,OWHIGH);
+        delay(1);
+
+      }
+
+
+
+      if(pulsesDetected == 1){
+        delay(4);
+        digitalWrite(leds[0],readCurrentValue());
+      }
+      if(pulsesDetected == 2){
+        delay(4);
+        digitalWrite(leds[1],readCurrentValue());
+      }
+      if(pulsesDetected == 3){
+        delay(4);
+        digitalWrite(leds[2],readCurrentValue());
+      }
+      if(pulsesDetected == 4){
+        delay(4);
+        digitalWrite(leds[3],readCurrentValue());
+        // Serial.println("nummer 96");
+        // Serial.println(readCurrentValue());
+      }
+
     }
   } else {
   }
